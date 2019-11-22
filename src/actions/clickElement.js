7ed0513ce9fa34b5787fa8e2c50bf2b5470414bd
mvilla <matteo.villa@chili.com>
2019-11-22 18:01:54 +0100

    simulatemouseover

--- src/actions/clickElement.js
+++ src/actions/clickElement.js
@@ -3,14 +3,37 @@ import returnElementByPath from '../utils/returnElementByPath';
 const clickElement = (data) => {
     try {
         const el = returnElementByPath(data.element)
-        const xPos = el.offsetLeft;
-        const yPos = el.offsetHeight;
+
+
+        const rect = el.getBoundingClientRect(),
+            xPos = rect.left,
+            yPos = rect.top;
+
+
+        console.log(xPos, yPos)
         console.log(document.elementFromPoint(xPos, yPos))
         document.elementFromPoint(xPos, yPos).click();
+        simulateMouseover(el)
         return 'ok';
     } catch (e) {
         throw e
     }
 }
+
+const simulateMouseover = (myTarget) => {
+    const event = new MouseEvent('mousemove', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': true
+    });
+    const canceled = !myTarget.dispatchEvent(event);
+    if (canceled) {
+        // A handler called preventDefault.
+    } else {
+        // None of the handlers called preventDefault.
+    }
+}
+
+
 
 export default clickElement;
